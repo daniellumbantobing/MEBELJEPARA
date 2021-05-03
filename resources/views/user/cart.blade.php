80bec3cc7a12ef263a5b19fdefad603884ba6c51
daniellumbantobing <daniellumbantobing05@gmail.com>
2021-05-03 15:14:41 +0700

    modify cart  url img

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -49,7 +49,7 @@
  
     <tr>
       <td> 
-        <a href="/produk/{{$cart->produk->id}}/detail" style="color: black; text-decoration: none;">
+        <a href="{{url('produk/'.$cart->produk->id.'/detail')}}" style="color: black; text-decoration: none;">
         <img src="{{url('images/'.$cart->produk->gambar)}}" class="card-img-top img-fluid">
         </a>
        {{$cart->produk->nama_produk}}  
